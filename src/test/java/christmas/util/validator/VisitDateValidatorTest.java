f0b84d267fcc1720fbbde79fde77f6df252a00f5
sotogito <108535526+sotogito@users.noreply.github.com>
2023-11-15 15:54:55 +0900

    feat : 날짜 예외 처리 테스트 코드 작성

---
+++ src/test/java/christmas/util/validator/VisitDateValidatorTest.java
@@ -0,0 +1,33 @@
+package christmas.util.validator;
+
+import christmas.domain.restaurant.Category;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class VisitDateValidatorTest {
+
+    @Test
+    @DisplayName("정상입력시 예외 처리 없음")
+    void orderMenuValidator() {
+        int date = 25;
+
+        assertDoesNotThrow(() -> VisitDateValidator.visitDateValidator(date));
+    }
+
+    @Test
+    @DisplayName("메뉴 20개 이상일시 예외 처리")
+    void isWithinRangeNum() {
+        int date = 32;
+
+        assertThatThrownBy(() -> VisitDateValidator.visitDateValidator(date))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+}
