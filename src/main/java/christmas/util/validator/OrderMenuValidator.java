b5856628b069aa5aaf029c6636ffa1a9a93e5a11
sotogito <108535526+sotogito@users.noreply.github.com>
2023-11-15 23:06:13 +0900

    chore : 출력메시지 변수명 변경

--- src/main/java/christmas/util/validator/OrderMenuValidator.java
+++ src/main/java/christmas/util/validator/OrderMenuValidator.java
@@ -8,14 +8,14 @@ import java.util.Map;
 public class OrderMenuValidator {
     private static final int MIN = 1;
     private static final int MAX = 20;
-    private static final String ERROR_ORDER_MENU = "메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.\n";
+    private static final String NOTICE_ORDER_MENU = "메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.\n";
     private static final String NOTICE_ONLY_BEVERAGE = "음료만 주문 시, 주문할 수 없습니다.\n";
 
     public static void orderMenuValidator(Map<Category, Integer> orderCategory) {
         int quantity = orderCategory.values().stream().mapToInt(Integer::intValue).sum();
 
         if (!isWithinRangeNum(quantity)) {
-            throw new IllegalArgumentException(ERROR_ORDER_MENU);
+            throw new IllegalArgumentException(NOTICE_ORDER_MENU);
         } else if (isOverMinQuantity(quantity)) {
             throw new IllegalArgumentException(InputView.ERROR_ORDER_MENU);
         } else if (isOnlyBeverage(orderCategory)) {
